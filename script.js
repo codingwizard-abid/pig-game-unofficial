65c3610bd47c53dcc226fe13a20a2b0269d3ff68
Mohammad Abid <68217349+abidzmohammad@users.noreply.github.com>
2022-08-27 21:31:08 +0600

    function added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function(){
+   const dice = Math.trunc(Math.random() * 6) + 1;
+   diceEl.src = `./dice-${dice}.png`;
+
+   //display dice
+   diceEl.classList.remove('hidden');
+})
